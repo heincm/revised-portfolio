30e7e45de90b5fbe4c5e44380937d46e41947d14
Chris <chris.m.hein@gmail.com>
2019-12-28 16:50:19 -0500

    using react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/HomePage'
 import LanguagesPage from './pages/LanguagesPage'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import PortfolioPage from './pages/PortfolioPage';
 
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/index">
           <HomePage />
@@ -19,7 +23,11 @@ function App() {
         <Route path="/languages">
           <LanguagesPage />
         </Route>
+        <Route path="/portfolio">
+          <PortfolioPage />
+        </Route>
       </Switch>
+      <Footer />
     </Router>
   );
 }
