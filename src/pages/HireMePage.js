0a6044cbad4a3b0eb073fb6286f59747174a62b8
Chris <chris.m.hein@gmail.com>
2021-05-28 09:40:49 -0500

    updated spacing for mobile vs desktop views

--- src/pages/HireMePage.js
+++ src/pages/HireMePage.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import '../pages/HireMePage.css'
+
 function HireMe() {
 
     const [width, setWidth] = useState(window.innerWidth);
@@ -19,22 +21,24 @@ function HireMe() {
         <>
             <title>Hire Me</title>
             <main>
-   
-                {isMobile && (
-                    <div className="container">
+
+                {isMobile &&
+                    (<div className="container">
                         <h1 className="center-align">Hire Me</h1>
-                        <p>I am Self-motivated Software Engineer with an excellent professional background, strong work ethic,
+                        <p>I am self-motivated Software Engineer with an excellent professional background, strong work ethic,
                         and a desire for continuous learning and development. Experienced in creating responsive front web and mobile
                         applications as well as optimizing and automating development processes. Highly skilled in managing multiple
                         projects simultaneously, collaborating with key stakeholders, and providing innovative solutions to complex problems.</p>
                         <p>To learn more about me, check out my <a href="/images/Website_Resume.pdf" >resume</a>.</p>
                         <p>You can also connect with me on <a href="http://www.linkedin.com/in/heincm">LinkedIn</a>.</p>
-                    </div>)}
-                {!isMobile && (<div>
-                    <object data="/images/Website_Resume.pdf" type="application/pdf" width="100%" height="700px" aria-label="Chris Hein Resume" />
-                </div>)}
+                    </div>)
+                }
 
-
+                {!isMobile &&
+                    (<div>
+                        <object data="/images/Website_Resume.pdf" type="application/pdf" width="100%" height="700px" aria-label="Chris Hein Resume" />
+                    </div>)
+                }
             </main>
         </>
     )
