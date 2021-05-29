3ff6f54cd345cd44bd1d4fb5cd35f90e1a570880
Chris <chris.m.hein@gmail.com>
2021-05-29 11:55:47 -0500

    added ant form

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,6 +1,23 @@
 import React from 'react';
-// TODO: Fix the issue of the labels being over placeholder
+import { Form, Input, Button } from 'antd';
+import 'antd/dist/antd.css';
+
 // TODO: Get this thing to actually send out emails
+
+const validateMessages = {
+    required: '${label} is required!',
+    types: {
+        email: '${label} is not a valid email!',
+        number: '${label} is not a valid number!',
+    },
+    number: {
+        range: '${label} must be between ${min} and ${max}',
+    },
+};
+
+const onFinish = (values) => {
+    console.log(values);
+};
 class Contact extends React.Component {
     componentDidMount() {
         document.title = "Contact Me | Chris Hein"
@@ -8,58 +25,100 @@ class Contact extends React.Component {
     render() {
         return (
             <main>
-            <div className="container contact-container">
-                <h1 className="center-align">Contact Me</h1>
-                <p>Use the form below to send me a message or email me at <a href="mailto:chris.m.hein@gmail.com?Subject=Website%20Email">chris.m.hein@gmail.com</a>.</p>
-                <div className="row">
-                    <form className="col s12">
-                        <div className="row">
-                            <div className="input-field col s6">
-                                <i className="material-icons prefix">account_circle</i>
-                                <input placeholder="John" id="first_name" type="text" className="validate" />
-                                <label htmlFor="first_name">First Name</label>
+                <div className="container contact-container">
+                    <h1 className="center-align">Contact Me</h1>
+                    <p>Use the form below to send me a message or email me at <a href="mailto:chris.m.hein@gmail.com?Subject=Website%20Email">chris.m.hein@gmail.com</a>.</p>
+                    <div className="row">
+                        <Form name="nest-messages"  onFinish={onFinish} validateMessages={validateMessages}>
+                            <Form.Item
+                                name={['user', 'name']}
+                                label="Name"
+                                rules={[
+                                    {
+                                        required: true,
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item
+                                name={['user', 'email']}
+                                label="Email"
+                                rules={[
+                                    {
+                                        type: 'email',
+                                        required: true
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item 
+                            name={['user', 'introduction']} 
+                            label="Introduction"
+                            rules={[
+                                {
+                                    required: true
+                                },
+                            ]}
+                            >
+                                <Input.TextArea />
+                            </Form.Item>
+                            <Form.Item 
+                            >
+                                <Button type="primary" htmlType="submit">
+                                    Submit
+        </Button>
+                            </Form.Item>
+                        </Form>
+                        {/* <form className="col s12">
+                            <div className="row">
+                                <div className="input-field col s6">
+                                    <i className="material-icons prefix">account_circle</i>
+                                    <input placeholder="John" id="first_name" type="text" className="validate" />
+                                    <label htmlFor="first_name">First Name</label>
+                                </div>
+                                <div className="input-field col s6">
+                                    <i className="material-icons prefix">account_circle</i>
+                                    <input placeholder="Smith" id="last_name" type="text" className="validate" />
+                                    <label htmlFor="last_name">Last Name</label>
+                                </div>
                             </div>
-                            <div className="input-field col s6">
-                                <i className="material-icons prefix">account_circle</i>
-                                <input placeholder="Smith" id="last_name" type="text" className="validate" />
-                                <label htmlFor="last_name">Last Name</label>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <i className="material-icons prefix">email</i>
+                                    <input id="email" type="email" className="validate" placeholder="you@youmail.com" />
+                                    <label htmlFor="email">Email Address</label>
+                                </div>
                             </div>
-                        </div>
-                        <div className="row">
-                            <div className="input-field col s12">
-                                <i className="material-icons prefix">email</i>
-                                <input id="email" type="email" className="validate" placeholder="you@youmail.com" />
-                                <label htmlFor="email">Email Address</label>
+
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <i className="material-icons prefix">mode_edit</i>
+                                    <textarea id="text" className="materialize-textarea"
+                                        placeholder="Write your message here..."></textarea>
+                                    <label htmlFor="text">Message</label>
+                                </div>
                             </div>
-                        </div>
-    
-                        <div className="row">
-                            <div className="input-field col s12">
-                                <i className="material-icons prefix">mode_edit</i>
-                                <textarea id="text" className="materialize-textarea"
-                                    placeholder="Write your message here..."></textarea>
-                                <label htmlFor="text">Message</label>
-                            </div>
-                        </div>
-                        <button className="btn waves-effect waves-light" type="submit" name="action">Submit
-                            <i className="material-icons right">send</i>
-                        </button>
-                        <div className="modal-trigger" href="#successModal"></div>
-                    </form>
+                            <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                        <i className="material-icons right">send</i>
+                            </button>
+                            <div className="modal-trigger" href="#successModal"></div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-    
-            {/* Modal Structure */}
-            <div id="successModal" className="modal">
-                <div className="modal-content">
-                    <h4 className="center-align">Message Sent!</h4>
-                    <p className="center-align">Thanks for reaching out.</p>
-                    <p className="center-align"> If you're not redirected to the home page in a few seconds, please <a href="/">click here</a>.</p>
+
+                <div id="successModal" className="modal">
+                    <div className="modal-content">
+                        <h4 className="center-align">Message Sent!</h4>
+                        <p className="center-align">Thanks for reaching out.</p>
+                        <p className="center-align"> If you're not redirected to the home page in a few seconds, please <a href="/">click here</a>.</p>
+                    </div>
+                </div> */}
+                    </div>
                 </div>
-            </div>
-    
-            {/* End Modal Structure */}
-        </main>
+
+            </main>
         )
     }
 }
