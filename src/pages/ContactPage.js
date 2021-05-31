ac104c22ec020e1e63b21c16b6ce7439e7e424b2
Chris <chris.m.hein@gmail.com>
2021-05-31 09:14:45 -0400

    removed unnecessary validation

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -6,12 +6,8 @@ import 'antd/dist/antd.css';
 const validateMessages = {
     required: '${label} is required!',
     types: {
-        email: '${label} is not a valid email!',
-        number: '${label} is not a valid number!',
-    },
-    number: {
-        range: '${label} must be between ${min} and ${max}',
-    },
+        email: '${label} is not a valid email!'
+    }
 };
 
 let apiLink = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/email' : 'https://chris-portfolio-backend-api.herokuapp.com/email'
